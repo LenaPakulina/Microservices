ac3b960403104bc87b3ac4d104b9102146a77266
Elena Pakulina <el.pakulina@yandex.ru>
2024-07-10 15:54:24 +0500

    Реализован модуль Source. мелкофикс

--- services/source/src/main/java/ru/skill/source/controller/TrackerController.java
+++ services/source/src/main/java/ru/skill/source/controller/TrackerController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @AllArgsConstructor
 @RestController
-@RequestMapping("api/item")
+@RequestMapping("/api/item")
 public class TrackerController {
     private final TrackerService trackerService;
 
