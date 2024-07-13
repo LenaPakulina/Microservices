738348b277f0fa852ea93bae884010bcba792d71
AzatIdrisov <azzzat72@yandex.ru>
2024-07-13 22:27:12 +0500

    Часть 4. Автобалансировка нагрузки

--- services-exchange/conversion/src/main/java/ru/microservice/conversion/service/CurrencyExchangeServiceProxy.java
+++ services-exchange/conversion/src/main/java/ru/microservice/conversion/service/CurrencyExchangeServiceProxy.java
@@ -1,11 +1,13 @@
 package ru.microservice.conversion.service;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import ru.microservice.conversion.model.CurrencyConversionBean;
 
-@FeignClient(name = "forex-service", url = "localhost:8000")
+@FeignClient(name = "forex-service")
+@RibbonClient(name="forex-service")
 public interface CurrencyExchangeServiceProxy {
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     CurrencyConversionBean retrieveExchangeValue(@PathVariable("from") String from,
